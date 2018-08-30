f7a073c4ba7e01cc6a9a1ee34a4818da0093ba7b
dellapi <dellapi@lab1-12.eng.utah.edu>
2018-08-30 12:09:41 -0600

    got some of the skeletan up and ready

--- match_hw/match_main.c
+++ match_hw/match_main.c
@@ -1,8 +1,30 @@
+/* Author: Adam Della-Piana, CS 4400, Match HW Assignment, 8/30/2018
+*
+*
+*/
+
+int read_arguments(int argc, char *argv[]); // The idea is that if it's not valid input it prints something out and exits the program
+void interpret_flag(int modifier); // This should read in a flag and adjust the settings
+void check_match();
+
 int main(int argc, char *argv[])
 {
-  printf("There are %d arguments\n", argc);
+  /* Read in arguments to prepare for processing */
+  int modifier = read_arguments(argc, argv);
 
-  for(int i =0; i < argc; i++)
-    printf("Arg %d is %s\n", i, argv[i]);
+  /* Adjust the settings based on the flags */
+  interpret_flag(modifier);
 
+  /* Process the string as specified by the user */
+
+}
+
+int read_arguments(int argc, char *argv[])
+{
+  return 0;
+}
+
+void interpret_flag(int modifier)
+{
+  printf("The modifier is %d\n", modifier);
 }
